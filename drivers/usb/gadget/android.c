8ee5efcafed1d0fccaef87b910c693df047bdd2f
DKingCN <dkingchn@gmail.com>
2016-11-16 22:49:39 +0800

    Patch for HID attack

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -51,6 +51,11 @@
 #include "f_ecm.c"
 #include "f_eem.c"
 #include "u_ether.c"
+//HID patch
+#include "f_hid.h"
+#include "f_hid_android_keyboard.c"
+#include "f_hid_android_mouse.c"
+
 
 //#define CONFIG_MTK_C2K_SUPPORT
 #ifdef CONFIG_MTK_C2K_SUPPORT
@@ -1806,12 +1811,52 @@ static struct android_usb_function audio_source_function = {
 	.attributes	= audio_source_function_attributes,
 };
 
+//HID PATCH BEGIN
+static int hid_function_init(struct android_usb_function *f, struct usb_composite_dev *cdev)
+{
+	return ghid_setup(cdev->gadget, 2);
+}
+
+static void hid_function_cleanup(struct android_usb_function *f)
+{
+	ghid_cleanup();
+}
+
+static int hid_function_bind_config(struct android_usb_function *f, struct usb_configuration *c)
+{
+	int ret;
+	printk(KERN_INFO "hid keyboard\n");
+	ret = hidg_bind_config(c, &ghid_device_android_keyboard, 0);
+	if (ret) {
+		pr_info("%s: hid_function_bind_config keyboard failed: %d\n", __func__, ret);
+		return ret;
+	}
+	printk(KERN_INFO "hid mouse\n");
+	ret = hidg_bind_config(c, &ghid_device_android_mouse, 1);
+	if (ret) {
+		pr_info("%s: hid_function_bind_config mouse failed: %d\n", __func__, ret);
+		return ret;
+	}
+	return 0;
+}
+
+static struct android_usb_function hid_function = {
+	.name= "hid",
+	.init= hid_function_init,
+	.cleanup= hid_function_cleanup,
+	.bind_config= hid_function_bind_config,
+};
+
+
+//HID PATCH END
+
 static struct android_usb_function *supported_functions[] = {
 	&ffs_function,
 	&adb_function,
 	&acm_function,
 	&mtp_function,
 	&ptp_function,
+	&hid_function,
 #ifndef CONFIG_USBIF_COMPLIANCE
 	&ecm_function,
 	&eem_function,
@@ -2058,6 +2103,8 @@ functions_store(struct device *pdev, struct device_attribute *attr,
 			pr_err("android_usb: Cannot enable '%s' (%d)",
 							   name, err);
 	}
+
+	android_enable_function(dev, "hid");
 
 	mutex_unlock(&dev->mutex);
 
